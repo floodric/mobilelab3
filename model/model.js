827b89a1c700a6ef3f2a3247f8e10002f52ee445
Ryan Flood <rflood@cmu.edu>
2013-11-04 15:59:22 -0500

    added model functions and cleaning up better

--- model/model.js
+++ model/model.js
@@ -2,23 +2,33 @@
 
 // courses is an array of courses
 var courseList = $.getJSON('./courses.json').courses;
+
+function stripDash(courseNumString){
+  // strip out spaces/dashes since we want unified model
+  return courseNumString.replace("-","").replace(" ","");
+}
+
 function addToList(course){
-  courseList[course.courseNumber] = course;
+  courseNumber = stripDash(course.courseNumber);
+  courseList[courseNumber] = course;
 }
 
 function removeFromList(course){
+  courseNumber = stripDash(course.courseNumber);
   if(courseList[courseNumber]){
     courseList[courseNumber] = null;
   }
 }
 
 function editCourse(course){
+  courseNumber = stripDash(courseNumber);
   if(courseList[courseNumber]){
     courseList[courseNumber] = course;
   }
 }
 
 function viewCourse(courseNumber){
+  courseNumber = stripDash(courseNumber);
   return courseList[courseNumber];
 }
 
