9a21fd6f828ed4ace3988b839b7f27bb5cf6367a
Ryan Flood <rflood@cmu.edu>
2013-11-04 15:50:16 -0500

    basic exports created

--- model/model.js
+++ model/model.js
@@ -1,1 +1,34 @@
 // empty file for editing
+
+// courses is an array of courses
+var courseList = $.getJSON('./courses.json');
+function addToList(course){
+  courseList[course.courseNumber] = course;
+}
+
+function removeFromList(course){
+  if(courseList[courseNumber]){
+    courseList[courseNumber] = null;
+  }
+}
+
+function editCourse(course){
+  if(courseList[courseNumber]){
+    courseList[courseNumber] = course;
+  }
+}
+
+function viewCourse(courseNumber){
+  return courseList[courseNumber];
+}
+
+function listCourses(){
+  return courseList;
+}
+
+exports.view = viewCourse;
+exports.edit = editCourse;
+exports.destroy = removeFromList();
+exports.create = addToList;
+exports.list = listCourses();
+
